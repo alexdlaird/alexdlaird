08f1eaf948a666df8c656e1f159fd6cfe2987fac
Alex Laird <contact@alexlaird.com>
2025-03-14 19:04:30 -0500

    Bash script cleanup for stability.

--- tools/install.sh
+++ tools/install.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-mkdir -p $HOME/Developer
+mkdir -p "$HOME/Developer"
 PROFILE_DIR="$HOME/Developer/alexdlaird"
 
 if [ -d "$PROFILE_DIR" ]; then
-  git -C $PROFILE_DIR fetch
-  git -C $PROFILE_DIR pull
+  git -C "$PROFILE_DIR" fetch
+  git -C "$PROFILE_DIR" pull
 else
-  git clone git@github.com:alexdlaird/alexdlaird.git $PROFILE_DIR
+  git clone git@github.com:alexdlaird/alexdlaird.git "$PROFILE_DIR"
 fi
 
-. $PROFILE_DIR/tools/init/dev-init-system
+. "$PROFILE_DIR/tools/init/dev-init-system"
 
-. $PROFILE_DIR/tools/lib/rc-pyenv.sh
+. "$PROFILE_DIR/tools/lib/rc-pyenv.sh"
 
-python $PROFILE_DIR/tools/bin/dev-repos-init
+python "$PROFILE_DIR/tools/bin/dev-repos-init"
 
 exec zsh
